a5ae09f42f201a61c939bfc03ba6e057f6e6e838
Nicolas Enea <44454886+nicoenea@users.noreply.github.com>
2021-10-18 09:57:29 +0800

    Add files via upload

---
+++ webstream.py
@@ -0,0 +1,85 @@
+#from motiondetect.singlemotiondetector import SingleMotionDetector
+from imutils.video import VideoStream
+from flask import Response
+from flask import Flask
+from flask import render_template
+import threading
+import argparse
+import datetime
+import imutils
+import time
+import cv2
+
+# initialize the output frame and a lock used to ensure thread-safe
+# exchanges of the output frames (useful when multiple browsers/tabs
+# are viewing the stream)
+outputFrame = None
+lock = threading.Lock()
+# initialize a flask object
+app = Flask(__name__)
+# initialize the video stream and allow the camera sensor to warmup
+vs = VideoStream(src=0).start()
+time.sleep(2.0)
+
+@app.route("/")
+def index():
+	# return the rendered template
+	return render_template("index.html")
+
+
+def readFrame():
+	# grab global references to the video stream, output frame, and
+	# lock variables
+	global vs, outputFrame, lock
+
+
+    # loop over frames from the video stream
+	while True:
+		# read the next frame from the video stream, resize it,
+		# convert the frame to grayscale, and blur it
+		frame = vs.read()
+		with lock:
+			outputFrame = frame.copy()
+
+def generate():
+	# grab global references to the output frame and lock variables
+	global outputFrame, lock
+	# loop over frames from the output stream
+	while True:
+		# wait until the lock is acquired
+		with lock:
+			# check if the output frame is available, otherwise skip
+			# the iteration of the loop
+			if outputFrame is None:
+				continue
+			# encode the frame in JPEG format
+			(flag, encodedImage) = cv2.imencode(".jpg", outputFrame)
+			# ensure the frame was successfully encoded
+			if not flag:
+				continue
+		# yield the output frame in the byte format
+		yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + 
+			bytearray(encodedImage) + b'\r\n')
+
+
+@app.route("/video_feed")
+def video_feed():
+	# return the response generated along with the specific media
+	# type (mime type)
+	return Response(generate(),
+		mimetype = "multipart/x-mixed-replace; boundary=frame")
+
+# check to see if this is the main thread of execution
+if __name__ == '__main__':
+	
+    
+    # start a thread that will perform motion detection
+    t = threading.Thread(target=readFrame)
+    t.daemon = True
+    t.start()
+	# start the flask app
+    
+    app.run(host="0.0.0.0", port="8000", debug=True,
+		threaded=True, use_reloader=False)
+# release the video stream pointer
+vs.stop()   
